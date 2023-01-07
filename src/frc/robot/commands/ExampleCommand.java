832236945bb261292a5d58c611be441be7e78ca4
teo67 <teogerst@gmail.com>
2023-01-07 14:34:01 -0800

    samples

---
+++ src/frc/robot/commands/ExampleCommand.java
@@ -0,0 +1,17 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ExampleCommand extends CommandBase {
+    public void initialize() {
+
+    }
+    public void execute() {
+
+    }
+    public boolean isFinished() {
+        return true;
+    }
+    public void end(boolean interrupted) {
+        
+    }
+}
---
+++ src/frc/robot/subsystems/ExampleSubsystem.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ExampleSubsystem extends SubsystemBase {
+    public void periodic() {
+
+    }
+    public void simulationPeriodic() {
+
+    }
+}
