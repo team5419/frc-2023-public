2e6a51d4555da660d8cdfad38b12ddaac027a122
aaron esterson <aaronesterson@students.berkeley.net>
2023-01-24 17:37:08 -0800

    added on to go to position

--- src/frc/robot/commands/goToPosition.java
+++ src/frc/robot/commands/goToPosition.java
@@ -16,26 +16,40 @@ public class goToPosition extends CommandBase {
     private Rotation2d rotation;
     double dirY;
     double dirX;
+    double speed;
     public goToPosition(ShuffleboardTab tab){
         joystick = new XboxController(1);
         rotation = new Rotation2d(0);
         desiredPosition = new Pose2d(14,2,rotation); // the desired position for the robot to move to
         vis = new Vision(tab);
         swerve = new Swerve(vis);
-        
+        speed = 100;
     }
     
     
     public void initialize() {
         dirX = desiredPosition.getX() - swerve.pose().getX();
         dirY = desiredPosition.getY() - swerve.pose().getY();
+        // if (dirX > dirY){//makes sure the direction isnt greater when robot is further away
+        //     dirX = 1;
+        //     dirY = dirY/dirX;
+        // }else{
+        //     dirY = 1;
+        //     dirX = dirX/dirY;
+        // }
+        // dirX *= speed;
+        // dirY *= speed;
     }
     public void execute() {
         swerve.drive(dirX, dirY,0);
         //swerves the robot over to the desired position
     }
     public boolean isFinished() {
-       return swerve.pose() == desiredPosition;//finishes command when at desired position
+        if (joystick.getBButton()){
+            return true;//stops command when B is pressed on controller
+        }else{
+            return swerve.pose() == desiredPosition;//finishes command when at desired position
+        }
     }
     public void end(boolean interrupted) {
        swerve.brake();// brakes the swerve when command is finished
