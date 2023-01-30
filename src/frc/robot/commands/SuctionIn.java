c9b595d73b7be2b8441b0a3072845641a1d5678b
lemmaammel <mayacobb2@gmail.com>
2023-01-29 16:33:50 -0800

    added timer to suction

--- src/frc/robot/commands/SuctionIn.java
+++ src/frc/robot/commands/SuctionIn.java
@@ -2,15 +2,21 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Suction;
 
+import edu.wpi.first.wpilibj.Timer;
+
 public class SuctionIn extends CommandBase {
     private Suction suction;
+    private Timer timer;
 
     public SuctionIn(Suction _suction){
         this.suction = _suction;
+        this.timer = new Timer();
         // addRequirements(this.suction);
     }
 
     public void initialize() {
+        this.timer.reset();
+        this.timer.start();
         this.suction.airIn();
     }
 
@@ -18,8 +24,9 @@ public class SuctionIn extends CommandBase {
     }
 
     public boolean isFinished() {
-        return true;
+        return this.timer.get() > 3.0;
     }
     public void end(boolean interrupted) {
+        this.timer.stop();
     }
 }
