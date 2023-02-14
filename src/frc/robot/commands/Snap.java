e8ecf29b93d1d8c73987b56a4b0554dd56da7dfc
jakob <jacobzwicker71@gmail.com>
2023-02-13 20:51:17 -0800

    fixxed mistake

--- src/frc/robot/commands/Snap.java
+++ src/frc/robot/commands/Snap.java
@@ -10,7 +10,7 @@ import frc.robot.subsystems.Vision;
 
 public class Snap extends SequentialCommandGroup {
     public Snap(Swerve drivetrain, Vision vision, XboxController driver) {
-        double deg = ((int)((((double)((driver.getPOV() + 45) % 360)) / 90) + 0.5)) * 90;
+        double deg = ((int)(((double)((driver.getPOV() + 45) % 360)) / 90)) * 90;
         addCommands(
             new RamseteSwerve(drivetrain, vision, new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(deg)), true, true)
         );
