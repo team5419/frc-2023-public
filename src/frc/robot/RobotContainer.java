e6bd302819854ab00ab15c9c6cdbcaf7db76572b
jakob <jacobzwicker71@gmail.com>
2023-02-13 20:40:01 -0800

    Snap to rotation

--- src/frc/robot/RobotContainer.java
+++ src/frc/robot/RobotContainer.java
@@ -21,48 +21,48 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class RobotContainer {
-    private GenericShootIntake cuber;
-    //private IntakeDeploy deploy;
-    public Vision vision;
-    private SendableChooser<Supplier<SequentialCommandGroup>> autoSelector;
-    private SendableChooser<Supplier<GenericShootIntake>> coneShooterSelector;
-    private SendableChooser<Supplier<GenericShootIntake>> cubeShooterSelector;
-  	//private Drivetrain drivetrain;
-  	private Swerve swerve;
-  	private XboxController driver;
-  	private XboxController codriver;
-  	//private Claw claw;
-  	private GenericShootIntake coner;
-  	private EverybotArm arm;
-    private PneumaticHub hub;
+	private GenericShootIntake cuber;
+	//private IntakeDeploy deploy;
+	public Vision vision;
+	private SendableChooser<Supplier<SequentialCommandGroup>> autoSelector;
+	private SendableChooser<Supplier<GenericShootIntake>> coneShooterSelector;
+	private SendableChooser<Supplier<GenericShootIntake>> cubeShooterSelector;
+	//private Drivetrain drivetrain;
+	private Swerve swerve;
+	private XboxController driver;
+	private XboxController codriver;
+	//private Claw claw;
+	private GenericShootIntake coner;
+	private EverybotArm arm;
+	private PneumaticHub hub;
 
 	private boolean setUp;
 
-  	public RobotContainer(ShuffleboardTab tab) {
+	public RobotContainer(ShuffleboardTab tab) {
 		setUp = false;
-    	driver = new XboxController(0);
-    	codriver = new XboxController(1);
-    	vision = new Vision(tab, true, true);
-    
-    	//claw = new Claw();
-    	arm = null; 
-    	coner = null;
-    	cuber = null; // to be set up later
-      hub = null;
-    
-    	swerve = new Swerve(vision, true); /* CHOOSE ONE!!! */
-    	//drivetrain = new Drivetrain(); /* ^^^ */
+		driver = new XboxController(0);
+		codriver = new XboxController(1);
+		vision = new Vision(tab, true, true);
+	
+		//claw = new Claw();
+		arm = null; 
+		coner = null;
+		cuber = null; // to be set up later
+		hub = null;
+	
+		swerve = new Swerve(vision, true); /* CHOOSE ONE!!! */
+		//drivetrain = new Drivetrain(); /* ^^^ */
 
-    	// = new IntakeDeploy();
-		
-    	autoSelector = new SendableChooser<Supplier<SequentialCommandGroup>>();
-    	tab.add("Auto selector", autoSelector).withSize(2, 1).withPosition(0, 0);
-    	autoSelector.setDefaultOption("Baseline", () -> new Baseline());
-    	//sautoSelector.addOption("Proto Routine", new ProtoRoutine(drivetrain));
-    	autoSelector.addOption("Proto Routine", () -> new SwerveRoutine(swerve, vision, coner, cuber));
+		// = new IntakeDeploy();
+	
+		autoSelector = new SendableChooser<Supplier<SequentialCommandGroup>>();
+		tab.add("Auto selector", autoSelector).withSize(2, 1).withPosition(0, 0);
+		autoSelector.setDefaultOption("Baseline", () -> new Baseline());
+		//sautoSelector.addOption("Proto Routine", new ProtoRoutine(drivetrain));
+		autoSelector.addOption("Proto Routine", () -> new SwerveRoutine(swerve, vision, coner, cuber));
 		autoSelector.addOption("Proto Routine with Vision", () -> new SwerveWithVision(swerve, vision, coner, cuber));
 		autoSelector.addOption("Go to zero", () -> new SequentialCommandGroup(
-			new RamseteSwerve(swerve, vision, new Pose2d(1.80, 1.02, new Rotation2d(0.0)), true, false)));
+		new RamseteSwerve(swerve, vision, new Pose2d(1.80, 1.02, new Rotation2d(0.0)), true, false)));
 
 		coneShooterSelector = new SendableChooser<Supplier<GenericShootIntake>>();
 		tab.add("Cone shooter", coneShooterSelector).withSize(2, 1).withPosition(2, 0);
@@ -73,7 +73,7 @@ public class RobotContainer {
 		cubeShooterSelector = new SendableChooser<Supplier<GenericShootIntake>>();
 		tab.add("Cube shooter", cubeShooterSelector).withSize(2, 1).withPosition(4, 0);
 		cubeShooterSelector.setDefaultOption("Low Cuber", () -> new Cuber(generateHub(), false));
-  	}
+	}
 
 	private EverybotArm generateArm() {
 		if(arm == null) {
@@ -100,22 +100,25 @@ public class RobotContainer {
 		}
 	}
   
-  	public void configureButtonBindings() {
+	public void configureButtonBindings() {
 		if(setUp) {
 			return;
 		}
-    	Trigger aButtonDriver = new Trigger(() -> driver.getAButton());
-    	Trigger bButtonDriver = new Trigger(() -> driver.getBButton());
-    	Trigger xButtonDriver = new Trigger(() -> driver.getXButton());
+		Trigger aButtonDriver = new Trigger(() -> driver.getAButton());
+		Trigger bButtonDriver = new Trigger(() -> driver.getBButton());
+		Trigger xButtonDriver = new Trigger(() -> driver.getXButton());
 		Trigger yButtonDriver = new Trigger(() -> driver.getYButton());
-    	Trigger rightTrigger = new Trigger(() -> driver.getRightTriggerAxis() > SwerveDriveConstants.triggerDeadband);
+		Trigger rightTrigger = new Trigger(() -> driver.getRightTriggerAxis() > SwerveDriveConstants.triggerDeadband);
 		Trigger leftTrigger = new Trigger(() -> driver.getLeftTriggerAxis() > SwerveDriveConstants.triggerDeadband);
-    	Trigger leftBumper = new Trigger(() -> driver.getLeftBumper());
-    	Trigger rightBumper = new Trigger(() -> driver.getRightBumper());
+		Trigger leftBumper = new Trigger(() -> driver.getLeftBumper());
+		Trigger rightBumper = new Trigger(() -> driver.getRightBumper());
+		Trigger dpad = new Trigger(() -> driver.getPOV() != -1);
 
 		Trigger rightTriggerCodriver = new Trigger(() -> codriver.getRightTriggerAxis() > SwerveDriveConstants.triggerDeadband);
 
-    	leftBumper.whileTrue(Commands.runEnd(() -> { swerve.slowMode = true; }, () -> { swerve.slowMode = false; }));
+		dpad.onTrue(new Snap(swerve, vision, driver));
+
+		leftBumper.whileTrue(Commands.runEnd(() -> { swerve.slowMode = true; }, () -> { swerve.slowMode = false; }));
     
 		// eventually, this is what the code will look like: 
 		rightBumper.onTrue(new AlignSetup(coner, cuber, swerve, vision, driver));
@@ -131,15 +134,15 @@ public class RobotContainer {
 			xButtonDriver.whileTrue(new InOut((Coner)coner));
 		}
 		yButtonDriver.onTrue(new ResetGyro(swerve));
-    	leftTrigger.whileTrue(new RunIntake(coner));
-    	rightTrigger.whileTrue(new RunIntake(cuber));
+		leftTrigger.whileTrue(new RunIntake(coner));
+		rightTrigger.whileTrue(new RunIntake(cuber));
 		rightTriggerCodriver.whileTrue(Commands.runEnd(() -> cuber.shoot(TargetHeights.INTAKE), () -> cuber.stop(TargetHeights.LOW), cuber.subsystem()));
 		//rightBumper.onTrue(new SpecialRamseteSwerve(swerve, vision, driver, coner, cuber, true));
 		// rightBumper.toggleOnTrue(Commands.runEnd(() -> arm.gotoPosition(Arm.outPosition), () -> arm.gotoPosition(Arm.inPosition)))
 		// bButtonDriver.onTrue(new Balance(swerve, driver));
 		// aButtonCodriver.toggleOnTrue(Commands.runOnce(() -> swerve.brake()));
 		setUp = true;
-  	}
+	}
 
 	public Command getAutonomousCommand() {
 		return autoSelector.getSelected().get();
---
+++ src/frc/robot/commands/Snap.java
@@ -0,0 +1,18 @@
+package frc.robot.commands;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.XboxController;
+// import edu.wpi.first.math.geometry.Pose2d;
+// import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Swerve;
+import frc.robot.subsystems.Vision;
+
+public class Snap extends SequentialCommandGroup {
+    public Snap(Swerve drivetrain, Vision vision, XboxController driver) {
+        double deg = ((int)((((double)((driver.getPOV() + 45) % 360)) / 90) + 0.5)) * 90;
+        addCommands(
+            new RamseteSwerve(drivetrain, vision, new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(deg)), true, true)
+        );
+    }
+}
