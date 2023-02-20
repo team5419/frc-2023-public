cd8d3e673df6460661e8bc0c2d9477b1800a5142
teo67 <teogerst@gmail.com>
2023-02-20 09:23:38 -0800

    stuff

--- src/frc/robot/commands/Snap.java
+++ src/frc/robot/commands/Snap.java
@@ -4,16 +4,28 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.XboxController;
 // import edu.wpi.first.math.geometry.Pose2d;
 // import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.classes.RamseteOptions;
 import frc.robot.subsystems.Swerve;
 import frc.robot.subsystems.Vision;
 
-public class Snap extends SequentialCommandGroup {
+public class Snap extends CommandBase {
+    Swerve drivetrain;
+    Vision vision;
+    XboxController driver;
     public Snap(Swerve drivetrain, Vision vision, XboxController driver) {
-        double deg = (((driver.getPOV() + 45) % 360) / 90) * 90;
-        addCommands(
-            new RamseteSwerve(drivetrain, vision, new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(deg)), new RamseteOptions(true, true))
-        );
+        this.drivetrain = drivetrain;
+        this.vision = vision;
+        this.driver = driver;
+    }
+
+    public void initialize() {
+        double deg = (((this.driver.getPOV() + 45) % 360) / 90) * 90;
+        RamseteSwerve ramsete = new RamseteSwerve(this.drivetrain, this.vision, new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(deg)), new RamseteOptions(true, true));
+        ramsete.schedule();
+    }
+
+    public boolean isFinished() {
+        return true;
     }
 }
